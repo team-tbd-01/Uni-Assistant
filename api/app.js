ad154dff8a34a8f19e9da23cd1680ec626b162d4
Reyder95 <khoutas98@gmail.com>
2020-12-01 18:06:15 -0500

    Implemented course adding to database - Still need to validate whether you leave anything blank or not. - Retrieves schools and courses and puts them in the dropdown.

--- api/app.js
+++ api/app.js
@@ -4,11 +4,16 @@ const morgan = require('morgan');
 const path = require('path');
 const db = require('./models');
 const app = express();
+const cors = require('cors');
 const PORT = process.env.PORT || 8000;
 
 
 // this lets us parse 'application/json' content in http requests
 app.use(bodyParser.json())
+
+// This lets us bypass cors
+
+app.use(cors())
 
 // add http request logging to help us debug and audit app use
 const logFormat = process.env.NODE_ENV==='production' ? 'combined' : 'dev';
