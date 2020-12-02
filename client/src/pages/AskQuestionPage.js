5e95f81b26aa2e5c0672962481bd9b1c7cd15897
aharonmaghen <aharon.maghen27@students.kbcc.cuny.edu>
2020-12-02 11:30:06 +0200

    Fix design bugs

--- client/src/pages/AskQuestionPage.js
+++ client/src/pages/AskQuestionPage.js
@@ -7,17 +7,19 @@ function handleClick() {
 
 function AboutUsPage(props) {
   return (
-    <form id="question">
-        <div className="form-group">
-            <label className="h3">Title:</label>
-            <input className="form-control" type="text" />
-        </div>
-        <div className="form-group">
-            <label className="h3">Question:</label>
-            <textarea className="form-control questionBox" form="question"></textarea>
-        </div>
-        <Link to="/a-course" className="btn btn-primary" onClick={handleClick}>Submit</Link>
-  </form>
+    <div className="text-center">
+      <form id="question">
+          <div className="form-group mx-auto w-50">
+              <label className="h3">Title:</label>
+              <input className="form-control mx-auto" type="text" />
+          </div>
+          <div className="form-group mx-auto w-50">
+              <label className="h3">Question:</label>
+              <textarea className="form-control questionBox" form="question" maxLength="1000"></textarea>
+          </div>
+          <Link to="/a-course" className="btn btn-primary" onClick={handleClick}>Submit</Link>
+    </form>
+  </div>
   );
 }
 
